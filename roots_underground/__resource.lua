811ff96e5f14145071e4a7312aefc0681ad6b554
djowroots <43259672+djowroots@users.noreply.github.com>
2018-10-30 07:11:21 -0300

    Create __resource.lua

---
+++ roots_underground/__resource.lua
@@ -0,0 +1,14 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'Gangue ESX com menus loot/etc'
+
+version '0.1.0 beta'
+
+server_scripts {
+	'@es_extended/locale.lua',
+	'sv_main.lua'
+}
+
+client_scripts {
+	'cl_main.lua'
+}
