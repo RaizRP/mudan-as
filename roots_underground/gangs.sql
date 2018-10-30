d90263ad143c2e14679ec3342e251ac6daa3c922
djowroots <43259672+djowroots@users.noreply.github.com>
2018-10-30 07:25:13 -0300

    Added sql

---
+++ roots_underground/gangs.sql
@@ -0,0 +1,7 @@
+INSERT INTO `jobs` (`name`, `label`) VALUES
+	('biker', 'Biker')
+;
+
+INSERT INTO `job_grades` (`job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+	('biker', 0, 'empregado', 'Temporário', 250, '{}', '{}'
+;
